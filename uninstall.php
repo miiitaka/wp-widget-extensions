b92e0861294680c238676aff3f2f27b111131683
Kazuya Takami <foundationmeister@outlook.com>
2016-06-13 21:10:03 +0900

    Added : View & Uninstall process.

---
+++ uninstall.php
@@ -0,0 +1,51 @@
+<?php
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+new Posted_Display_Uninstall();
+
+/**
+ * Plugin Uninstall
+ *
+ * @author  Kazuya Takami
+ * @version 1.1.0
+ * @since   1.1.0
+ */
+class Posted_Display_Uninstall {
+
+	/**
+	 * Constructor Define.
+	 *
+	 * @version 1.1.0
+	 * @since   1.1.0
+	 */
+	public function __construct () {
+		$this->delete_widget_meta( "widget_meta" );
+	}
+
+	/**
+	 * Delete Widget Meta Option.
+	 *
+	 * @version 1.1.0
+	 * @since   1.1.0
+	 * @param   string $option
+	 */
+	private function delete_widget_meta ( $option ) {
+		$widget_array = get_option( $option );
+		$update_array = array();
+
+		foreach ( $widget_array as $key => $value ) {
+			if ( is_array( $value ) ) {
+				unset(
+					$value["site_admin"],
+					$value["site_login"],
+					$value["entries_rss"],
+					$value["comments_rss"],
+					$value["wordpress_org"]
+				);
+			}
+			$update_array[$key] = $value;
+		}
+		update_option( $option, $update_array );
+	}
+}
