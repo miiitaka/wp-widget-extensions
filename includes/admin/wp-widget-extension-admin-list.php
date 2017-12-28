11279fbcb494522e6b666d54499d6dbe9d0a5fc3
Kazuya Takami <foundationmeister@outlook.com>
2017-12-28 16:44:45 +0900

    Information display after update. #98

--- includes/admin/wp-widget-extension-admin-list.php
+++ includes/admin/wp-widget-extension-admin-list.php
@@ -3,7 +3,7 @@
  * Schema.org Admin List
  *
  * @author  Kazuya Takami
- * @version 2.0.0
+ * @version 2.0.4
  * @since   2.0.0
  */
 class WP_Widget_Extension_List {
@@ -53,18 +53,23 @@ class WP_Widget_Extension_List {
 	/**
 	 * LIST Page HTML Render.
 	 *
-	 * @version 2.0.0
+	 * @version 2.0.4
 	 * @since   2.0.0
 	 */
 	private function page_render () {
 		$html  = '';
 		$html .= '<div class="wrap">';
-		$html .= '<h1>' . esc_html__( 'Widget Extension Settings', $this->text_domain );
-		$html .= '</h1>';
+		$html .= '<h1>' . esc_html__( 'Widget Extension Settings', $this->text_domain ) . '</h1>';
+		$html .= '<hr>';
 		echo $html;
 
-		$html  = '<hr>';
-		$html .= '<form action="" method="POST">';
+		if ( isset( $_POST[$this->text_domain] ) && $_POST[$this->text_domain] ) {
+			if ( check_admin_referer( $this->text_domain . '-key', $this->text_domain ) ) {
+				$this->information_render();
+			}
+		}
+
+		$html  = '<form action="" method="POST">';
 		$html .= '<table class="wp-list-table widefat fixed striped posts">';
 		$html .= '<tr>';
 		$html .= '<th scope="row" style="width: 100px;">' . __( 'Status', $this->text_domain ) . '</th>';
@@ -106,5 +111,22 @@ class WP_Widget_Extension_List {
 				update_option( $this->text_domain, $_POST['widget'] );
 			}
 		}
+	}
+
+	/**
+	 * Information Message Render
+	 *
+	 * @version 2.0.4
+	 * @since   1.0.0
+	 */
+	private function information_render () {
+		$html  = '<div id="message" class="updated notice notice-success is-dismissible below-h2">';
+		$html .= '<p>Widget Extensions Information Update.</p>';
+		$html .= '<button type="button" class="notice-dismiss">';
+		$html .= '<span class="screen-reader-text">Widget Extensions Information Update.</span>';
+		$html .= '</button>';
+		$html .= '</div>';
+
+		echo $html;
 	}
 }
